ba97fceaeba72cdda626bb3e47648289571f7808
lukavr05 <lvrooyen@yahoo.com>
2025-06-10 10:22:38 +0100

    updated navbar to have bold elements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,74 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
+import { useState } from 'react';
+import { AppBar, Toolbar, Button } from '@mui/material'
 
-const Navbar = () => {
+function Navbar() {
+  const [selected, setSelected] = useState(null);
+
+  const handleSelect = (id) => {
+    setSelected(id);
+  }
+
   return (
     <AppBar 
       position='sticky' 
       color='primary'
       sx={{ top: 0, width:'100vw', left: 0 }}
     >
-      <Toolbar>
-        <Button color='inherit' href="/" >Home</Button>
-        <Button color='inherit' href="/about">About</Button>
-        <Button color='inherit' href="/music">Music</Button>
+      <Toolbar
+        sx={{ gap: 2,
+            '& button': {
+              textTransform: 'none',
+              borderRadius: '8px',
+              boxShadow: 'none', 
+              '&:hover': {
+                boxShadow: 'none', 
+              },
+            },
+          }}
+      >
+        <Button 
+            color='inherit' 
+            href="/"
+            onClick={() => handleSelect('home')}
+            sx={{
+              fontWeight: selected === 'home' ? 'bold' : 'normal',
+            }}
+          >
+            Home
+        </Button>
+        <Button 
+            color='inherit' 
+            href="/about"
+            onClick={() => handleSelect('about')}
+            sx={{
+              fontWeight: selected === 'about' ? 'bold' : 'normal',
+            }}
+          >
+            About
+        </Button>
+        <Button 
+            color='inherit' 
+            href="/music"
+            onClick={() => handleSelect('music')}
+            sx={{
+              fontWeight: selected === 'music' ? 'bold' : 'normal',
+            }}
+          >
+            Music
+        </Button>
+        <Button 
+            color='inherit' 
+            href="/media"
+            onClick={() => handleSelect('media')}
+            sx={{
+              fontWeight: selected === 'media' ? 'bold' : 'normal',
+            }}
+          >
+            Media
+        </Button>
       </Toolbar>
     </AppBar>
   );
-};
+}
 
 export default Navbar;
