81c0ebb35c9cd7e543330dd5f9959db4b49ebd09
lukavr05 <lvrooyen@yahoo.com>
2025-06-20 20:51:44 +0100

    added routes to pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
+import Events from './pages/Events';
+import Media from './pages/Media';
+import Portfolio from './pages/Portfolio';
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/media" element={<Media />} />
+        <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
     </>
   )
